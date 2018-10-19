ddfc8910d135f0f77b7f75d279719898b564d119
lingyielia <31973683+lingyielia@users.noreply.github.com>
2018-10-19 02:13:10 -0400

    add

--- sql/answers.sql
+++ sql/answers.sql
@@ -1,3 +1,19 @@
+-- show all tables
+select *
+from pg_catalog.pg_tables
+where schemaname != 'pg_catalog' and
+      schemaname != 'information_schema';
+
+      -- public	employees	postgres		false	false	false	false
+      -- public	orderdetails	postgres		false	false	false	false
+      -- public	orders	postgres		false	false	false	false
+      -- public	categories	postgres		false	false	false	false
+      -- public	customergroupthresholds	postgres		false	false	false	false
+      -- public	customers	postgres		false	false	false	false
+      -- public	shippers	postgres		false	false	false	false
+      -- public	products	postgres		false	false	false	false
+      -- public	suppliers	postgres		false	false	false	false
+
 -- 1.
 select * from shippers;
 
@@ -96,3 +112,107 @@ join shippers s
 on o.shipvia = s.shipperid
 where orderid < 10270
 order by orderid;
+
+----- Medium -----
+------------------
+
+-- 1.
+select c.categoryname, p.totalnumber
+from categories c
+join
+(
+  select categoryid, count(productid) as totalnumber
+  from products
+  group by categoryid
+) p
+on p.categoryid = c.categoryid
+order by totalnumber desc;
+
+-- 2.
+select country, city, count(*)
+from customers
+group by country, city;
+
+-- 3.
+select * from products
+where unitsinstock <= reorderlevel
+order by productid;
+
+-- 4.
+select * from products
+where (unitsinstock + unitsonorder) <= reorderlevel and
+      discontinued = 'false'
+order by productid;
+
+-- 5.
+select *,
+       case
+       when region is null then 0
+       else 1
+       end as hasregion
+from customers
+order by hasregion;
+
+-- 6.
+select shipcountry, avg(freight::money::numeric::float8) as avg_freight
+from orders
+group by shipcountry
+order by avg_freight desc
+limit 3;
+
+-- 7.
+select shipcountry, avg(freight::money::numeric::float8) as avg_freight
+from orders
+where date_part('year', orderdate) = '2015'
+group by shipcountry
+order by avg_freight desc
+limit 3;
+
+-- 8.
+select shipcountry, avg(freight::money::numeric::float8) as avg_freight
+from orders
+where date_trunc('month', orderdate)::date in
+  (
+  select distinct(date_trunc('month', t.orderdate)::date) as month_t
+  from orders t
+  order by month_t desc
+  limit 12
+  )
+group by shipcountry
+order by avg_freight desc
+limit 3;
+
+-- 9.
+select o.orderid, od.quantity, p.productname, e.employeeid, e.lastname
+from orders o
+
+join
+orderdetails od
+on o.orderid = od.orderid
+
+join
+products p
+on p.productid = od.productid
+
+join
+employees e
+on o.employeeid = e.employeeid
+order by o.orderid, p.productid;
+
+-- 10.
+select *
+from customers c
+left join
+orders o
+on c.customerid = o.customerid
+where o.orderid is null;
+
+-- 11.
+select *
+from customers c
+where c.customerid not in
+(
+  select distinct(customerid)
+  from orders o2
+  where o2.employeeid = 4
+);
